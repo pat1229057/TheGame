798ae3b4ca44e9681deb396413474b552d79f078
pat1229057 <patrickq2222@gmail.com>
2026-01-02 16:55:17 -0800

    implemented some of the assets methods

--- src/Assets.cpp
+++ src/Assets.cpp
@@ -1,6 +1,8 @@
 #include "Assets.h"
 #include "Animation.hpp"
 #include "SFML/Graphics.hpp"
+#include "SFML/Graphics/Font.hpp"
+#include "SFML/Graphics/Texture.hpp"
 #include <cassert>
 #include <fstream>
 #include <iostream>
@@ -12,18 +14,32 @@ Assets &Assets::Instance() {
 }
 
 void Assets::addTexture(const std::string &textureName, const std::string &path,
-                        bool smooth) {}
+                        bool smooth) {
+  m_textureMap[textureName] = sf::Texture(path, smooth);
+}
 
 // random size_t
 void Assets::addAnimation(const std::string &AnimationName,
                           const std::string &path, size_t) {}
 
-void Assets::addFont(const std::string &fontName, const std::string &path) {}
+void Assets::addFont(const std::string &fontName, const std::string &path) {
+  m_fontMap[fontName] = sf::Font(path);
+}
 
 void Assets::loadFromFile(const std::string &path) {}
 
-const sf::Texture &Assets::getTexture(const std::string &textureName) const {}
-const Animation &Assets::getAnimation(const std::string &animationName) const {}
-const sf::Font &Assets::getFont(const std::string &fontName) const {}
-const std::map<std::string, sf::Texture> &Assets::getTextures() const {}
-const std::map<std::string, Animation> &Assets::getAnimations() const {}
+const sf::Texture &Assets::getTexture(const std::string &textureName) const {
+  return m_textureMap.at(textureName);
+}
+const Animation &Assets::getAnimation(const std::string &animationName) const {
+  return m_animationMap.at(animationName);
+}
+const sf::Font &Assets::getFont(const std::string &fontName) const {
+  return m_fontMap.at(fontName);
+}
+const std::map<std::string, sf::Texture> &Assets::getTextures() const {
+  return m_textureMap;
+}
+const std::map<std::string, Animation> &Assets::getAnimations() const {
+  return m_animationMap;
+}
